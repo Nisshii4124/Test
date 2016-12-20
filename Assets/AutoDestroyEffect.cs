293ed232bc2a6ff9474164fd2e514c04cb1d7f84
DaikiNishiwaki <nkc.daiki.nishiwaki@gmail.com>
2016-12-20 12:43:27 +0900

    test

---
+++ Assets/AutoDestroyEffect.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDestroyEffect : MonoBehaviour
+{
+    //パーティクルシステムの参照
+    //キャッシュ
+    ParticleSystem particle;
+
+    // Use this for initialization
+    void Start()
+    {
+        //リンクをはって捕まえておく
+        particle = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // パーティクルの再生が終了したらGameObjectを削除
+        // パーティクルシステムが再生終了しているか？(isPlayingを見に行く)isPlayingはフラグになっている
+        if (particle.isPlaying == false)
+        {
+            Destroy(gameObject);
+        }
+        /*
+        if (particle = GetComponent<ParticleSystem>().isPlaying == false)
+        {
+            Destroy(gameObject);
+        }
+        StartとUpdate内の文を1つにしたもの
+        これだと毎回データを探すときに頭から総当たりで探してしまう
+        */
+    }
+}
